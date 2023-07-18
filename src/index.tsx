c577758e58f288acc375240fdc1336e0d6ff4683
DucLVN <nhanduc811@gmail.com>
2023-07-18 15:37:26 +0700

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 import Homebar from './module/public/homebar/homebar';
 
-import { PrivateRouter, PublicRouter } from './app';
+import { PublicRouter } from './module/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
